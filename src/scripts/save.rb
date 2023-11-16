d9f3126bdf1f3cf29e1f7b7344d462853193a0bb
Teddy Juhlin-Henricson <43953682+c0ldbear@users.noreply.github.com>
2023-11-16 14:54:34 +0100

    Add script to create wsg-info.json and related github actions (#2)

---
+++ src/scripts/save.rb
@@ -0,0 +1,12 @@
+require './wsg-info'
+require 'json'
+
+wsg_json = get_wsg_info()
+
+filePath = "../../public/" + Constant::FILENAME+".json"
+
+File.open(filePath, "wb") { |json|
+    json.write(JSON.pretty_generate(wsg_json))
+}
+
+puts ">> Finished!"
