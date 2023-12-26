b8321b37fd08dac01fe62535a45708e9a0809c46
Anna Viklund <annamariaviklund@gmail.com>
2023-12-26 16:40:37 +0100

    Add success criteria to guideline cards

--- src/components/GuidelineCard/GuidelineCard.tsx
+++ src/components/GuidelineCard/GuidelineCard.tsx
@@ -1,22 +1,33 @@
 import { ExternalLinkIcon } from "@radix-ui/react-icons";
 import { Guideline } from "../../models/guideline";
+import { levelToEmojis } from "../../utils/levelToLabel";
+import { AccordionItem, AccordionRoot } from "../Accordion/Accordion";
 import { LinkButton } from "../Button/Button";
 import { Tag } from "../Tag/Tag";
-import { levelToEmojis } from "../../utils/levelToLabel";
 import styles from "./GuidelineCard.module.css";
 
 export const GuidelineCard = ({ guideline }: { guideline: Guideline }) => (
   <div className={styles.wrapper}>
     <span className={styles.category}>{guideline.category.title}</span>
-    <div className={styles.title}>
-      <h3>{guideline.title}</h3>
-    </div>
+    <h3 className={styles.title}>{guideline.title}</h3>
     <div className={styles.actions}>
       <LinkButton to={guideline.url}>
         To guideline <ExternalLinkIcon />
       </LinkButton>
     </div>
     <p className={styles.description}>{guideline.description}</p>
+    <div className={styles.details}>
+      <AccordionRoot>
+        <AccordionItem value="success-criteria" title="Success criteria">
+          {guideline.criteria.map((criterion, index) => (
+            <div key={index} className={styles.criterion}>
+              <h4>{criterion.title}</h4>
+              <p>{criterion.description}</p>
+            </div>
+          ))}
+        </AccordionItem>
+      </AccordionRoot>
+    </div>
     <div className={styles.tags}>
       <div className={styles.extra}>
         <Tag
