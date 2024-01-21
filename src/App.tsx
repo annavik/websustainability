e24f72fdd3a2937e123b8d7a979d216872a7551c
Anna Viklund <annamariaviklund@gmail.com>
2024-01-21 15:49:02 +0100

    Remove lib @tanstack/react-query

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,18 @@
-import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import styles from "./App.module.css";
 import { Guidelines } from "./components/Guidelines/Guidelines";
 import { Header } from "./components/Header/Header";
 import { BookmarksContextProvider } from "./utils/bookmarks/bookmarksContext";
 
-const queryClient = new QueryClient();
-
 function App() {
   return (
-    <QueryClientProvider client={queryClient}>
-      <BookmarksContextProvider>
-        <Header />
-        <main className={styles.main}>
-          <div className={styles.content}>
-            <Guidelines />
-          </div>
-        </main>
-      </BookmarksContextProvider>
-    </QueryClientProvider>
+    <BookmarksContextProvider>
+      <Header />
+      <main className={styles.main}>
+        <div className={styles.content}>
+          <Guidelines />
+        </div>
+      </main>
+    </BookmarksContextProvider>
   );
 }
 
